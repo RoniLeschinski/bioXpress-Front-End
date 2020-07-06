7e6b6b43c6557c0e7ab55fee52a85f6985e68958
RoniLeschinski <ronileschinski@gmail.com>
2020-07-06 18:32:32 -0300

    .

--- screens/Producto.js
+++ screens/Producto.js
@@ -40,6 +40,7 @@ export default function Producto({navigation, route}) {
         </TouchableOpacity>
       </View>
     </View>
+    //<View styles={{backgroundColor: "blue", width: "100%", height: "50%"}}></View>
   );
 }
 
